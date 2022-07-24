210f73c155afe66257cb96c2bb04c203308a8163
JINNY <103866627+yeyae@users.noreply.github.com>
2022-07-24 19:52:24 +0900

    Update 27.py

---
+++ 27.py
@@ -0,0 +1,9 @@
+#elif : if와 else로만 표현하기 힘들 때 사용
+pocket = ['paper', 'cellphone','money']
+card = True
+if 'money' in pocket:
+    print("택시를 타고 가라")
+elif card:
+    print("택시를 타고 가라")
+else:
+    print("걸어 가라")
